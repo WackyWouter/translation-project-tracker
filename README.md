8fc7f997e46dfaf8a997369fab78f2e46548106b
Wouter Bosch <wfcbosch@gmail.com>
2022-10-28 00:09:11 +0100

    framework setup

---
+++ README.md
@@ -0,0 +1,1 @@
+A Codeigniter 4 website made for my girlfriend to help her track her translation projects for her work.
---
+++ other/Project tracker.sql
@@ -0,0 +1,26 @@
+CREATE TABLE `users` (
+  `id` int AUTO_INCREMENT,
+  `uuid` varchar(255) UNIQUE PRIMARY KEY,
+  `username` varchar(255) UNIQUE NOT NULL,
+  `email` varchar(255) UNIQUE NOT NULL,
+  `password` varbinary NOT NULL,
+  `adddate` datetime,
+  `moddate` datetime
+);
+
+CREATE TABLE `projects` (
+  `id` int(11) AUTO_INCREMENT,
+  `uuid` varchar(255) UNIQUE,
+  `user_uuid` varchar(255),
+  `title` varchar(255) NOT NULL,
+  `source_language` varchar(255),
+  `target_language` varchar(255),
+  `planned_date` datetime, -- This will determine on which day the task is shown
+  `start_date` datetime,
+  `due_date` datetime,
+  `word_count` int(11),
+  `adddate` datetime,
+  `moddate` datetime
+);
+
+ALTER TABLE `projects` ADD FOREIGN KEY (`user_uuid`) REFERENCES `users` (`uuid`);
