6a8cfa18498d57f3ae3a36add6bbcf3c30c0e12d
Wouter Bosch <wouter@blocksonline.co.uk>
2022-11-17 17:04:42 +0000

    fix account not saving

--- public/assets/js/account.js
+++ public/assets/js/account.js
@@ -66,3 +66,62 @@ function validatePasswords() {
 
     return noErrorFound;
 }
+
+function saveAccount() {
+    let data = $("#editAccountForm").serializeArray();
+
+    // Empty old errors
+    $(".form-control").removeClass("is-invalid");
+    $(".invalid-feedback").html("");
+    $(".main-error").html("");
+
+    // if (validatePasswords()) {
+    $.ajax({
+        type: "POST",
+        url: "/account/save",
+        data: data,
+        dataType: "JSON",
+        success: function (data) {
+            if (data.status == "ok") {
+                window.location.href = "/account/profile";
+            } else {
+                Object.keys(data.errors).forEach(function (key) {
+                    $(key).find(".invalid-feedback").html(data.errors[key]);
+                    $(key).find("input").addClass("is-invalid");
+                });
+            }
+        },
+        error: function (data) {
+            $(".main-error").html(
+                "Something has gone wrong. Please try again later."
+            );
+        },
+    });
+    // }
+}
+
+function validateAccount() {
+    let noErrorFound = true;
+    const username = $("#username").val();
+    const email = $("#email").val();
+
+    // Check if username is valid
+    if (!/^[a-zA-Z0-9]{5,}$/.test(username)) {
+        $(".usernameField")
+            .find(".invalid-feedback")
+            .html("Please enter a valid username.");
+        $(".usernameField").find("input").addClass("is-invalid");
+        noErrorFound = false;
+    }
+
+    // Check if email is valid
+    if (!validateEmail(email)) {
+        $(".emailField")
+            .find(".invalid-feedback")
+            .html("Please enter a valid email.");
+        $(".emailField").find("input").addClass("is-invalid");
+        noErrorFound = false;
+    }
+
+    return noErrorFound;
+}
